8edca3a9460f7a9e54e6c6e98770a613aa540ef2
joooonh <116352772+joooonh@users.noreply.github.com>
2023-04-29 16:31:52 +0900

    Update 주유소.java

--- 백준/Silver/13305. 주유소/주유소.java
+++ 백준/Silver/13305. 주유소/주유소.java
@@ -4,25 +4,23 @@ public class Main {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
+        int[] km = new int[N - 1];
+        int[] oil = new int[N]; 
         
-        int[] road = new int[N - 1];        // 도로 길이
-        int[] oil = new int[N];             // 기름값
-        
-        for(int i=0; i<road.length; i++) {
-            road[i] = sc.nextInt();
+        for(int i=0; i<km.length; i++) {
+            km[i] = sc.nextInt();
         }
         for(int i=0; i<oil.length; i++) {
             oil[i] = sc.nextInt();
         }
         
-        int price = 0;
-        int min = oil[0];                   // 처음 기름값이니까 얘가 최소값
-        for(int i=0; i<road.length; i++) {
+        long price = 0;
+        long min = oil[0];
+        for(int i=0; i<km.length; i++) {
             if(oil[i] < min)
                 min = oil[i];
-            price += (min*road[i]);         // 기름의 최소값 * 도로길이
+            price += km[i] * min;
         }
-        
         System.out.println(price);
     }
 }
