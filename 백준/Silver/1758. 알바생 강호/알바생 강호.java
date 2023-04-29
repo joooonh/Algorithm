3b91dbc22b07b4d8a30ca3a6dac06efce327f264
joooonh <116352772+joooonh@users.noreply.github.com>
2023-04-29 18:01:26 +0900

    [Silver IV] Title: 알바생 강호, Time: 920 ms, Memory: 104892 KB -BaekjoonHub

---
+++ 백준/Silver/1758. 알바생 강호/알바생 강호.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();                   // 사람수 N명
+        Integer[] plan = new Integer[N];        // 주려고 했던 팁 (내림차순 정렬하려고 Integer로 받음)
+        int[] order = new int[N];
+        for(int i=0; i<N; i++) {
+            plan[i] = sc.nextInt();             // 주려고 했던 팁 배열 채움
+            order[i] = i;                       // 순서-1 
+        }
+        
+        Arrays.sort(plan, Collections.reverseOrder());  // 주려고 했던 팁 내림차순 정렬
+        
+        long tip = 0;
+        for(int i=0; i<N; i++) {
+            int minus = plan[i] - order[i];     // 주려고 했던 팁 - 순서
+            if(minus > 0) {
+                tip += minus;
+            }
+        }
+        System.out.println(tip);
+    }
+}
