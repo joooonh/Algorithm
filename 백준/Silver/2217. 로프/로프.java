91f7864d1c3f41a4d998141ce54dc47b2d196377
joooonh <116352772+joooonh@users.noreply.github.com>
2023-04-29 15:47:34 +0900

    Update 로프.java

--- 백준/Silver/2217. 로프/로프.java
+++ 백준/Silver/2217. 로프/로프.java
@@ -1,10 +1,10 @@
-import java.util.*; 
+import java.util.*;
 
 public class Main {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
-        int[] arr = new int[N];
+        int[] arr = new int[N]; 
         
         for(int i=0; i<N; i++) {
             arr[i] = sc.nextInt();
@@ -13,15 +13,11 @@ public class Main {
         Arrays.sort(arr);
         
         int max = 0;
-        if(N == 1) {
-            max = arr[0];
-        } else {
-            for(int i=0; i<N; i++) {
-                int weight = arr[i] * (N-i);  // i번째로 작은 로프부터 사용하는 경우의 최대 중량
-                max = Math.max(max, weight);
-            }
+        for(int i=0; i<N; i++) {
+            int weight = arr[i] * (N - i);
+            if(max < weight)
+                max = weight;
         }
-        
-        System.out.println(max); 
+        System.out.println(max);
     }
 }
