796f003c40b19c21d92ca3eb926d217fb53d75e2
joooonh <116352772+joooonh@users.noreply.github.com>
2023-08-04 19:19:02 +0900

    [Silver IV] Title: 돌 게임 2, Time: 216 ms, Memory: 17752 KB -BaekjoonHub

--- 백준/Silver/9656. 돌 게임 2/돌 게임 2.java
+++ 백준/Silver/9656. 돌 게임 2/돌 게임 2.java
@@ -5,7 +5,19 @@ public class Main {
         Scanner sc = new Scanner(System.in); 
         int n = sc.nextInt(); 
         
-        String answer = (n % 2 == 0) ? "SK" : "CY"; 
-        System.out.println(answer);
+        int[] dp = new int[1001]; 
+        
+        // 기본값 세팅 (상근승 = 0, 창영승 = 1)
+        dp[1] = 1;
+        dp[2] = 0; 
+        dp[3] = 1;
+        
+        // 1개 남았을 때, 3개 남았을 때 누가 이기는지 확인
+        for(int i=4; i<=n; i++) {
+            dp[i] = (dp[i - 3] == 1 && dp[i - 1] == 1) ? 0 : 1;
+        }
+        
+        System.out.println(dp[n] == 0 ? "SK" : "CY");
+        
     }
 }
