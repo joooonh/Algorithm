0d97b1cf836d9019edd102d40fb4ee2bc2c71799
joooonh <116352772+joooonh@users.noreply.github.com>
2023-08-25 19:45:32 +0900

    [level 1] Title: 3진법 뒤집기, Time: 10.19 ms, Memory: 75 MB -BaekjoonHub

--- 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.java
+++ 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -2,20 +2,15 @@ import java.util.*;
 
 class Solution {
     public int solution(int n) {
-        List<Integer> list = new ArrayList<>();
-        int answer = 0;
-        int j = 1;
+        String str = ""; 
         
         while(n > 0) {
-            list.add(n%3);   // {0, 0, 2, 1}
+            str = (n % 3) + str;
             n /= 3;
         }
         
-        for(int i = list.size() - 1; i >= 0; i--) {
-            answer += list.get(i) * j;
-            j *= 3;
-        }
+        str = new StringBuilder(str).reverse().toString();
         
-        return answer;
+        return Integer.parseInt(str, 3); 
     }
 }
