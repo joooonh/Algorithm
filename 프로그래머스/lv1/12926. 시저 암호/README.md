f5fcd4cbe2fd7accd741e99262473c1ff3764a60
joooonh <116352772+joooonh@users.noreply.github.com>
2023-08-28 21:50:17 +0900

    [level 1] Title: 시저 암호, Time: 1.49 ms, Memory: 77.7 MB -BaekjoonHub

--- 프로그래머스/lv1/12926. 시저 암호/README.md
+++ 프로그래머스/lv1/12926. 시저 암호/README.md
@@ -1,10 +1,10 @@
 # [level 1] 시저 암호 - 12926 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12926) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12926?language=java) 
 
 ### 성능 요약
 
-메모리: 74.7 MB, 시간: 3.01 ms
+메모리: 77.7 MB, 시간: 1.49 ms
 
 ### 구분
 
--- 프로그래머스/lv1/12926. 시저 암호/시저 암호.java
+++ 프로그래머스/lv1/12926. 시저 암호/시저 암호.java
@@ -3,28 +3,15 @@ import java.util.*;
 class Solution {
     public String solution(String s, int n) {
         StringBuilder sb = new StringBuilder();
-        char[] small = new char[26];
-        char[] capital = new char[26]; 
-        
-        for(int i=0; i<26; i++) {
-            small[i] = (char) (i + 97);
-            capital[i] = (char) (i + 65);
-        }
         
         for(char c : s.toCharArray()) {
-            if (97 <= c && c <= 122) {
-                for(int i=0; i<small.length; i++) {
-                    if (c == small[i])
-                        sb.append(small[(i + n)%26]);
-                }
-            } else if (65 <= c && c <= 90) {
-                for(int i=0; i<capital.length; i++) {
-                    if (c == capital[i])
-                        sb.append(capital[(i + n)%26]);
-                }
-            } else {
+            if (c == ' ') {
                 sb.append(" ");
-            }
+            } else if ('a' <= c && c <= 'z') {
+                sb.append((char)('a' + (c + n - 'a')%26));
+            } else if ('A' <= c && c <= 'Z') {
+                sb.append((char)('A' + (c + n - 'A')%26));
+            } 
         }
         
         return sb.toString();
